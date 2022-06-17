621421aa751c33ffde498d857bc8a54260c39230
Alberto Muñoz <albertomunozbautista@gmail.com>
2022-06-18 00:39:51 +0200

    Cargar generos

---
+++ AMusic/FrmListaCancionesGenero.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace AMusic
+{
+    partial class FrmListaCancionesGenero
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmListaCancionesGenero));
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties5 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties6 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties7 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties8 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            this.txtBuscar = new Bunifu.UI.WinForms.BunifuTextBox();
+            this.lstCancion = new System.Windows.Forms.ListView();
+            this.lblGenero = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.AcceptsReturn = false;
+            this.txtBuscar.AcceptsTab = false;
+            this.txtBuscar.AnimationSpeed = 200;
+            this.txtBuscar.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.txtBuscar.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.txtBuscar.BackColor = System.Drawing.Color.Transparent;
+            this.txtBuscar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("txtBuscar.BackgroundImage")));
+            this.txtBuscar.BorderColorActive = System.Drawing.Color.DodgerBlue;
+            this.txtBuscar.BorderColorDisabled = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.txtBuscar.BorderColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            this.txtBuscar.BorderColorIdle = System.Drawing.Color.Silver;
+            this.txtBuscar.BorderRadius = 30;
+            this.txtBuscar.BorderThickness = 1;
+            this.txtBuscar.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.txtBuscar.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBuscar.DefaultFont = new System.Drawing.Font("Segoe UI", 9.25F);
+            this.txtBuscar.DefaultText = "";
+            this.txtBuscar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.txtBuscar.ForeColor = System.Drawing.Color.White;
+            this.txtBuscar.HideSelection = true;
+            this.txtBuscar.IconLeft = null;
+            this.txtBuscar.IconLeftCursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBuscar.IconPadding = 10;
+            this.txtBuscar.IconRight = null;
+            this.txtBuscar.IconRightCursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBuscar.Lines = new string[0];
+            this.txtBuscar.Location = new System.Drawing.Point(67, 87);
+            this.txtBuscar.MaxLength = 32767;
+            this.txtBuscar.MinimumSize = new System.Drawing.Size(1, 1);
+            this.txtBuscar.Modified = false;
+            this.txtBuscar.Multiline = false;
+            this.txtBuscar.Name = "txtBuscar";
+            stateProperties5.BorderColor = System.Drawing.Color.DodgerBlue;
+            stateProperties5.FillColor = System.Drawing.Color.Empty;
+            stateProperties5.ForeColor = System.Drawing.Color.Empty;
+            stateProperties5.PlaceholderForeColor = System.Drawing.Color.Empty;
+            this.txtBuscar.OnActiveState = stateProperties5;
+            stateProperties6.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            stateProperties6.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            stateProperties6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            stateProperties6.PlaceholderForeColor = System.Drawing.Color.DarkGray;
+            this.txtBuscar.OnDisabledState = stateProperties6;
+            stateProperties7.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            stateProperties7.FillColor = System.Drawing.Color.Empty;
+            stateProperties7.ForeColor = System.Drawing.Color.Empty;
+            stateProperties7.PlaceholderForeColor = System.Drawing.Color.Empty;
+            this.txtBuscar.OnHoverState = stateProperties7;
+            stateProperties8.BorderColor = System.Drawing.Color.Silver;
+            stateProperties8.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            stateProperties8.ForeColor = System.Drawing.Color.White;
+            stateProperties8.PlaceholderForeColor = System.Drawing.Color.Empty;
+            this.txtBuscar.OnIdleState = stateProperties8;
+            this.txtBuscar.Padding = new System.Windows.Forms.Padding(3);
+            this.txtBuscar.PasswordChar = '\0';
+            this.txtBuscar.PlaceholderForeColor = System.Drawing.Color.Silver;
+            this.txtBuscar.PlaceholderText = "Buscar";
+            this.txtBuscar.ReadOnly = false;
+            this.txtBuscar.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtBuscar.SelectedText = "";
+            this.txtBuscar.SelectionLength = 0;
+            this.txtBuscar.SelectionStart = 0;
+            this.txtBuscar.ShortcutsEnabled = true;
+            this.txtBuscar.Size = new System.Drawing.Size(478, 45);
+            this.txtBuscar.Style = Bunifu.UI.WinForms.BunifuTextBox._Style.Bunifu;
+            this.txtBuscar.TabIndex = 7;
+            this.txtBuscar.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtBuscar.TextMarginBottom = 0;
+            this.txtBuscar.TextMarginLeft = 3;
+            this.txtBuscar.TextMarginTop = 0;
+            this.txtBuscar.TextPlaceholder = "Buscar";
+            this.txtBuscar.UseSystemPasswordChar = false;
+            this.txtBuscar.WordWrap = true;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // lstCancion
+            // 
+            this.lstCancion.BackColor = System.Drawing.Color.Black;
+            this.lstCancion.ForeColor = System.Drawing.Color.White;
+            this.lstCancion.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lstCancion.HideSelection = false;
+            this.lstCancion.Location = new System.Drawing.Point(67, 160);
+            this.lstCancion.Name = "lstCancion";
+            this.lstCancion.Size = new System.Drawing.Size(614, 436);
+            this.lstCancion.TabIndex = 6;
+            this.lstCancion.UseCompatibleStateImageBehavior = false;
+            this.lstCancion.View = System.Windows.Forms.View.Details;
+            this.lstCancion.DoubleClick += new System.EventHandler(this.lstCancion_DoubleClick);
+            // 
+            // lblGenero
+            // 
+            this.lblGenero.AutoSize = true;
+            this.lblGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenero.ForeColor = System.Drawing.Color.White;
+            this.lblGenero.Location = new System.Drawing.Point(60, 17);
+            this.lblGenero.Name = "lblGenero";
+            this.lblGenero.Size = new System.Drawing.Size(109, 39);
+            this.lblGenero.TabIndex = 5;
+            this.lblGenero.Text = "label1";
+            // 
+            // FrmListaCancionesGenero
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1020, 634);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.lstCancion);
+            this.Controls.Add(this.lblGenero);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmListaCancionesGenero";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmListaCancionesGenero";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.UI.WinForms.BunifuTextBox txtBuscar;
+        private System.Windows.Forms.ListView lstCancion;
+        private System.Windows.Forms.Label lblGenero;
+    }
+}
---
+++ AMusic/FrmListaCancionesGenero.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+using AMusic.Clases;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net;
+using System.Collections;
+
+namespace AMusic
+{
+    public partial class FrmListaCancionesGenero : Form
+    {
+        private String idGenero;
+        private FrmMenuUser frmMenuUser;
+        ImageList imageList = new ImageList();
+        ArrayList listaIdCanciones = new ArrayList();
+        ArrayList listaCanciones = new ArrayList();
+        int index = 0;
+        public FrmListaCancionesGenero(String idGenero, FrmMenuUser frmMenuUser)
+        {
+            InitializeComponent();
+            this.idGenero = idGenero;
+            this.frmMenuUser = frmMenuUser;
+            cargar();
+            cargarComponentes();
+            cargarLista();
+
+        }
+
+        private void cargarLista()
+        {
+            FirebaseResponse res = cliente.Get(@"Cancion");
+            Dictionary<string, Cancion> data = JsonConvert.DeserializeObject<Dictionary<string, Cancion>>(res.Body.ToString());
+            rellenarLista(data);
+
+        }
+
+        private void rellenarLista(Dictionary<string, Cancion> data)
+        {
+            listaIdCanciones.Clear();
+            lstCancion.Clear();
+            lstCancion.Columns.Clear();
+            listaCanciones.Clear();
+
+
+            foreach (var item in data)
+            {
+
+                if (item.Value.genero == idGenero)
+                {
+                    listaIdCanciones.Add(item.Value.idCancion);
+
+                    Cancion c = new Cancion(item.Value.idCancion, item.Value.nombre, item.Value.cancion, item.Value.caratula, item.Value.genero, item.Value.artista, item.Value.fecha); ;
+                    listaCanciones.Add(c);
+
+                    WebClient wc = new WebClient();
+                    byte[] bytes = wc.DownloadData(item.Value.caratula);
+                    MemoryStream ms = new MemoryStream(bytes);
+                    System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+                    ms.Dispose();
+
+                    this.imageList.Images.Add(img);
+                }
+            }
+
+            this.imageList.ImageSize = new Size(100, 100);
+            this.lstCancion.View = View.Details;
+            lstCancion.Columns.Add("", 300);
+
+
+            lstCancion.SmallImageList = imageList;
+            int cont = 0;
+            foreach (var item in data)
+            {
+                if (item.Value.genero == idGenero)
+                {
+                    lstCancion.Items.Add(item.Value.nombre, cont);
+                    cont++;
+
+                }
+
+            }
+        }
+
+        private void cargarComponentes()
+        {
+            FirebaseResponse res = cliente.Get(@"Genero/" + idGenero);
+            Genero genero = res.ResultAs<Genero>();
+
+            this.lblGenero.Text = genero.nombre;
+        }
+
+        IFirebaseConfig ifc = new FirebaseConfig()
+        {
+            AuthSecret = "eqBhoT5oymn26ukYh7HfYGFx568xn9MIK8jrzo7a",
+            BasePath = "https://amusicprueba-default-rtdb.europe-west1.firebasedatabase.app/"
+        };
+
+        IFirebaseClient cliente;
+
+        private void cargar()
+        {
+            try
+            {
+                cliente = new FireSharp.FirebaseClient(ifc);
+            }
+            catch (Exception)
+            {
+
+                throw; //no internet connection
+            }
+        }
+
+
+        private void recuperarCancion()
+        {
+
+            int fila = lstCancion.FocusedItem.Index;
+            index = fila;
+            /*
+            String idCancion = listaIdCanciones[fila].ToString();
+
+            FirebaseResponse res = cliente.Get(@"Cancion/" + idCancion);
+            Cancion cancion = res.ResultAs<Cancion>();*/
+
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            String texto = txtBuscar.Text.ToLower();
+
+
+            FirebaseResponse res = cliente.Get(@"Cancion");
+            Dictionary<string, Cancion> data = JsonConvert.DeserializeObject<Dictionary<string, Cancion>>(res.Body.ToString());
+            rellenarLista(data);
+
+            listaIdCanciones.Clear();
+            lstCancion.Clear();
+            lstCancion.Columns.Clear();
+            listaCanciones.Clear();
+
+            foreach (var item in data)
+            {
+
+                if (item.Value.genero == idGenero && item.Value.nombre.ToLower().Contains(texto))
+                {
+                    listaIdCanciones.Add(item.Value.idCancion);
+
+                    Cancion c = new Cancion(item.Value.idCancion, item.Value.nombre, item.Value.cancion, item.Value.caratula, item.Value.genero, item.Value.artista, item.Value.fecha); ;
+                    listaCanciones.Add(c);
+
+                    WebClient wc = new WebClient();
+                    byte[] bytes = wc.DownloadData(item.Value.caratula);
+                    MemoryStream ms = new MemoryStream(bytes);
+                    System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+                    ms.Dispose();
+
+                    this.imageList.Images.Add(img);
+                }
+            }
+
+            this.imageList.ImageSize = new Size(100, 100);
+            this.lstCancion.View = View.Details;
+            lstCancion.Columns.Add("", 300);
+
+
+            lstCancion.SmallImageList = imageList;
+            int cont = 0;
+            foreach (var item in data)
+            {
+                if (item.Value.genero == idGenero && item.Value.nombre.ToLower().Contains(texto))
+                {
+                    lstCancion.Items.Add(item.Value.nombre, cont);
+                    cont++;
+
+                }
+
+            }
+        }
+
+        private void lstCancion_DoubleClick(object sender, EventArgs e)
+        {
+            recuperarCancion();
+            FrmReproductorMusica frmReproducirMusica = new FrmReproductorMusica(listaCanciones, index);
+            frmReproducirMusica.MdiParent = frmMenuUser;
+            frmReproducirMusica.Dock = DockStyle.Fill;
+            frmReproducirMusica.Show();
+        }
+    }
+}
